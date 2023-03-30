0b373574d67440664e06907d6ac7de399fa011ed
Aaricdev <aaricdev@simply-stream.com>
2023-03-30 02:26:39 +0200

    fix: Added missing components to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
-export {default as TwitchClips, type TwitchClipProps } from './components/TwitchClip';
-export {default as TwitchRandomClips, type TwitchRandomClipsProps } from './components/TwitchRandomClips';
+export { default as TwitchClips, type TwitchClipProps } from './components/TwitchClip';
+export { default as TwitchRandomClips, type TwitchRandomClipsProps } from './components/TwitchRandomClips';
+export { default as ClipHeader, type ClipHeaderProps } from './components/ClipHeader';
+export { default as ClipTitle } from './components/ClipTitle';
+export { default as GameName, type GameNameProps } from './components/GameName';
+export { default as StreamerName } from './components/StreamerName';
